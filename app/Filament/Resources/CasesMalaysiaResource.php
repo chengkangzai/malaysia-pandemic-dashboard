74066a88990aee54d0f7d75f7ede3bcee41eee21
Ching Cheng Kang <pycck@hotmail.com>
2023-09-17 02:13:58 +0800

    improve UI UX

--- app/Filament/Resources/CasesMalaysiaResource.php
+++ app/Filament/Resources/CasesMalaysiaResource.php
@@ -31,12 +31,28 @@ class CasesMalaysiaResource extends Resource
             ->columns(3)
             ->schema([
                 TextEntry::make('date')->badge()->date(),
-                TextEntry::make('cases_new')->numeric(),
-                TextEntry::make('cases_cumulative')->numeric(),
-                TextEntry::make('cases_recovered_cumulative')->numeric(),
-                TextEntry::make('cases_recovered')->numeric(),
-                TextEntry::make('cases_active')->numeric(),
-                TextEntry::make('cases_cluster')->numeric(),
+
+                TextEntry::make('cases_new')
+                    ->label('New Cases')
+                    ->numeric(),
+                TextEntry::make('cases_cumulative')
+                    ->label('Cumulative Cases')
+                    ->numeric(),
+                TextEntry::make('cases_recovered')
+                    ->label('Recovered (New)')
+                    ->numeric(),
+                TextEntry::make('cases_recovered_cumulative')
+                    ->label('Cumulative Recovered')
+                    ->numeric(),
+                TextEntry::make('cases_active')
+                    ->label('Active Cases')
+                    ->numeric(),
+                TextEntry::make('cases_cluster')
+                    ->label('Cluster Cases')
+                    ->numeric(),
+                TextEntry::make('cases_import')
+                    ->label('Imported Cases')
+                    ->numeric(),
 
                 Section::make('Vaccination Status')
                     ->compact()
@@ -44,10 +60,18 @@ class CasesMalaysiaResource extends Resource
                     ->collapsed()
                     ->columns(3)
                     ->schema([
-                        TextEntry::make('cases_unvax')->numeric(),
-                        TextEntry::make('cases_pvax')->numeric(),
-                        TextEntry::make('cases_fvax')->numeric(),
-                        TextEntry::make('cases_boost')->numeric(),
+                        TextEntry::make('cases_unvax')
+                            ->label('Unvaccinated')
+                            ->numeric(),
+                        TextEntry::make('cases_pvax')
+                            ->label('Partially Vaccinated')
+                            ->numeric(),
+                        TextEntry::make('cases_fvax')
+                            ->label('Fully Vaccinated')
+                            ->numeric(),
+                        TextEntry::make('cases_boost')
+                            ->label('Booster Dose')
+                            ->numeric(),
                     ]),
 
                 Section::make('Age Group')
@@ -56,10 +80,18 @@ class CasesMalaysiaResource extends Resource
                     ->collapsed()
                     ->columns(3)
                     ->schema([
-                        TextEntry::make('cases_child')->numeric(),
-                        TextEntry::make('cases_adolescent')->numeric(),
-                        TextEntry::make('cases_adult')->numeric(),
-                        TextEntry::make('cases_elderly')->numeric(),
+                        TextEntry::make('cases_child')
+                            ->label('Children')
+                            ->numeric(),
+                        TextEntry::make('cases_adolescent')
+                            ->label('Adolescents')
+                            ->numeric(),
+                        TextEntry::make('cases_adult')
+                            ->label('Adults')
+                            ->numeric(),
+                        TextEntry::make('cases_elderly')
+                            ->label('Elderly')
+                            ->numeric(),
                     ]),
 
                 Section::make('Age Range')
@@ -68,30 +100,30 @@ class CasesMalaysiaResource extends Resource
                     ->collapsed()
                     ->columns(3)
                     ->schema([
-                        TextEntry::make('cases_0_4')->numeric(),
-                        TextEntry::make('cases_5_11')->numeric(),
-                        TextEntry::make('cases_12_17')->numeric(),
-                        TextEntry::make('cases_18_29')->numeric(),
-                        TextEntry::make('cases_30_59')->numeric(),
-                        TextEntry::make('cases_60_69')->numeric(),
-                        TextEntry::make('cases_70_79')->numeric(),
-                        TextEntry::make('cases_80')->numeric(),
-                    ]),
-
-                Section::make('Cluster')
-                    ->compact()
-                    ->collapsible()
-                    ->collapsed()
-                    ->columns(3)
-                    ->schema([
-                        TextEntry::make('cases_import')->numeric(),
-                        TextEntry::make('cluster_import')->numeric(),
-                        TextEntry::make('cluster_religious')->numeric(),
-                        TextEntry::make('cluster_community')->numeric(),
-                        TextEntry::make('cluster_highRisk')->numeric(),
-                        TextEntry::make('cluster_education')->numeric(),
-                        TextEntry::make('cluster_detentionCentre')->numeric(),
-                        TextEntry::make('cluster_workplace')->numeric(),
+                        TextEntry::make('cases_0_4')
+                            ->label('0-4 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_5_11')
+                            ->label('5-11 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_12_17')
+                            ->label('12-17 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_18_29')
+                            ->label('18-29 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_30_59')
+                            ->label('30-59 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_60_69')
+                            ->label('60-69 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_70_79')
+                            ->label('70-79 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_80')
+                            ->label('80 Years Old and Above')
+                            ->numeric(),
                     ]),
             ]);
     }
@@ -100,6 +132,7 @@ class CasesMalaysiaResource extends Resource
     {
         return $table
             ->defaultSort('date', 'desc')
+            ->columnToggleFormColumns(2)
             ->columns([
                 Tables\Columns\TextColumn::make('date')
                     ->date()
@@ -125,102 +158,127 @@ class CasesMalaysiaResource extends Resource
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_import')
+                    ->label('Imported Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_cluster')
+                    ->label('Cluster Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_unvax')
+                    ->label('Unvaccinated Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_pvax')
+                    ->label('Partially Vaccinated Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_fvax')
+                    ->label('Fully Vaccinated Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_boost')
+                    ->label('Booster Dose Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_child')
+                    ->label('Children')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_adolescent')
+                    ->label('Adolescents')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_adult')
+                    ->label('Adults')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_elderly')
+                    ->label('Elderly')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_0_4')
+                    ->label('0-4 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_5_11')
+                    ->label('5-11 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_12_17')
+                    ->label('12-17 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_18_29')
+                    ->label('18-29 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_30_59')
+                    ->label('30-59 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_60_69')
+                    ->label('60-69 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_70_79')
+                    ->label('70-79 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_80')
+                    ->label('80 Years Old and Above')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_import')
+                    ->label('Imported Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_religious')
+                    ->label('Religious Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_community')
+                    ->label('Community Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_highRisk')
+                    ->label('High Risk Group Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_education')
+                    ->label('Education Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_detentionCentre')
+                    ->label('Detention Centre Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_workplace')
+                    ->label('Workplace Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
--- app/Filament/Resources/CasesStateResource.php
+++ app/Filament/Resources/CasesStateResource.php
@@ -32,13 +32,28 @@ class CasesStateResource extends Resource
             ->schema([
                 TextEntry::make('date')->badge()->date(),
                 TextEntry::make('state')->badge()->color('info'),
-                TextEntry::make('cases_new')->numeric(),
-                TextEntry::make('cases_cumulative')->numeric(),
-                TextEntry::make('cases_recovered_cumulative')->numeric(),
-                TextEntry::make('cases_recovered')->numeric(),
-                TextEntry::make('cases_active')->numeric(),
-                TextEntry::make('cases_cluster')->numeric(),
-                TextEntry::make('cases_import')->numeric(),
+
+                TextEntry::make('cases_new')
+                    ->label('New Cases')
+                    ->numeric(),
+                TextEntry::make('cases_cumulative')
+                    ->label('Cumulative Cases')
+                    ->numeric(),
+                TextEntry::make('cases_recovered')
+                    ->label('Recovered (New)')
+                    ->numeric(),
+                TextEntry::make('cases_recovered_cumulative')
+                    ->label('Cumulative Recovered')
+                    ->numeric(),
+                TextEntry::make('cases_active')
+                    ->label('Active Cases')
+                    ->numeric(),
+                TextEntry::make('cases_cluster')
+                    ->label('Cluster Cases')
+                    ->numeric(),
+                TextEntry::make('cases_import')
+                    ->label('Imported Cases')
+                    ->numeric(),
 
                 Section::make('Vaccination Status')
                     ->compact()
@@ -46,10 +61,18 @@ class CasesStateResource extends Resource
                     ->collapsed()
                     ->columns(3)
                     ->schema([
-                        TextEntry::make('cases_unvax')->numeric(),
-                        TextEntry::make('cases_pvax')->numeric(),
-                        TextEntry::make('cases_fvax')->numeric(),
-                        TextEntry::make('cases_boost')->numeric(),
+                        TextEntry::make('cases_unvax')
+                            ->label('Unvaccinated')
+                            ->numeric(),
+                        TextEntry::make('cases_pvax')
+                            ->label('Partially Vaccinated')
+                            ->numeric(),
+                        TextEntry::make('cases_fvax')
+                            ->label('Fully Vaccinated')
+                            ->numeric(),
+                        TextEntry::make('cases_boost')
+                            ->label('Booster Dose')
+                            ->numeric(),
                     ]),
 
                 Section::make('Age Group')
@@ -58,10 +81,18 @@ class CasesStateResource extends Resource
                     ->collapsed()
                     ->columns(3)
                     ->schema([
-                        TextEntry::make('cases_child')->numeric(),
-                        TextEntry::make('cases_adolescent')->numeric(),
-                        TextEntry::make('cases_adult')->numeric(),
-                        TextEntry::make('cases_elderly')->numeric(),
+                        TextEntry::make('cases_child')
+                            ->label('Children')
+                            ->numeric(),
+                        TextEntry::make('cases_adolescent')
+                            ->label('Adolescents')
+                            ->numeric(),
+                        TextEntry::make('cases_adult')
+                            ->label('Adults')
+                            ->numeric(),
+                        TextEntry::make('cases_elderly')
+                            ->label('Elderly')
+                            ->numeric(),
                     ]),
 
                 Section::make('Age Range')
@@ -70,14 +101,30 @@ class CasesStateResource extends Resource
                     ->collapsed()
                     ->columns(3)
                     ->schema([
-                        TextEntry::make('cases_0_4')->numeric(),
-                        TextEntry::make('cases_5_11')->numeric(),
-                        TextEntry::make('cases_12_17')->numeric(),
-                        TextEntry::make('cases_18_29')->numeric(),
-                        TextEntry::make('cases_30_59')->numeric(),
-                        TextEntry::make('cases_60_69')->numeric(),
-                        TextEntry::make('cases_70_79')->numeric(),
-                        TextEntry::make('cases_80')->numeric(),
+                        TextEntry::make('cases_0_4')
+                            ->label('0-4 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_5_11')
+                            ->label('5-11 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_12_17')
+                            ->label('12-17 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_18_29')
+                            ->label('18-29 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_30_59')
+                            ->label('30-59 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_60_69')
+                            ->label('60-69 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_70_79')
+                            ->label('70-79 Years Old')
+                            ->numeric(),
+                        TextEntry::make('cases_80')
+                            ->label('80 Years Old and Above')
+                            ->numeric(),
                     ]),
             ]);
     }
@@ -86,6 +133,7 @@ class CasesStateResource extends Resource
     {
         return $table
             ->defaultSort('date', 'desc')
+            ->columnToggleFormColumns(3)
             ->columns([
                 Tables\Columns\TextColumn::make('date')
                     ->date()
@@ -113,118 +161,144 @@ class CasesStateResource extends Resource
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_import')
+                    ->label('Imported Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_cluster')
+                    ->label('Cluster Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_unvax')
+                    ->label('Unvaccinated Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_pvax')
+                    ->label('Partially Vaccinated Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_fvax')
+                    ->label('Fully Vaccinated Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_boost')
+                    ->label('Booster Dose Cases')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_child')
+                    ->label('Children')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_adolescent')
+                    ->label('Adolescents')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_adult')
+                    ->label('Adults')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_elderly')
+                    ->label('Elderly')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_0_4')
+                    ->label('0-4 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_5_11')
+                    ->label('5-11 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_12_17')
+                    ->label('12-17 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_18_29')
+                    ->label('18-29 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_30_59')
+                    ->label('30-59 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_60_69')
+                    ->label('60-69 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_70_79')
+                    ->label('70-79 Years Old')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cases_80')
+                    ->label('80 Years Old and Above')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_import')
+                    ->label('Imported Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_religious')
+                    ->label('Religious Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_community')
+                    ->label('Community Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_highRisk')
+                    ->label('High Risk Group Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_education')
+                    ->label('Education Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_detentionCentre')
+                    ->label('Detention Centre Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('cluster_workplace')
+                    ->label('Workplace Cluster')
                     ->toggleable(isToggledHiddenByDefault: true)
                     ->numeric()
                     ->sortable(),
             ])
+            ->paginationPageOptions([10, 25, 50, 100])
             ->filters([
                 Filter::make('date')
                     ->form([
                         DatePicker::make('from'),
                         DatePicker::make('until'),
                     ])
-                    ->query(fn (Builder $query, array $data): Builder => $query
+                    ->query(fn(Builder $query, array $data): Builder => $query
                         ->when($data['from'],
-                            fn (Builder $query, $date): Builder => $query->whereDate('date', '>=', $date),
+                            fn(Builder $query, $date): Builder => $query->whereDate('date', '>=', $date),
                         )
                         ->when($data['until'],
-                            fn (Builder $query, $date): Builder => $query->whereDate('date', '<=', $date),
+                            fn(Builder $query, $date): Builder => $query->whereDate('date', '<=', $date),
                         )),
 
                 Tables\Filters\SelectFilter::make('state')
@@ -241,9 +315,9 @@ class CasesStateResource extends Resource
                                 30 => '1 Month',
                             ]),
                     ])
-                    ->query(fn (Builder $query, array $data): Builder => $query
+                    ->query(fn(Builder $query, array $data): Builder => $query
                         ->when($data['range'],
-                            fn (Builder $query, $range): Builder => $query->whereDate('date', '>=', now()->subDays($range)),
+                            fn(Builder $query, $range): Builder => $query->whereDate('date', '>=', now()->subDays($range)),
                         )),
             ], Tables\Enums\FiltersLayout::AboveContentCollapsible)
             ->actions([
