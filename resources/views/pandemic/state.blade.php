dd5fe33fd22b6cf572d11a3e5b061754d8bde5c3
Ching Cheng Kang <pycck@hotmail.com>
2023-09-16 22:59:47 +0800

    format code

--- resources/views/pandemic/state.blade.php
+++ resources/views/pandemic/state.blade.php
@@ -4,9 +4,9 @@
     @include('partial.rocket')
     <div class="mx-2">
         <livewire:pandemic-state.head :state="$state" />
-{{--        <livewire:pandemic-state.dashboard :state="$state" />--}}
-{{--        <livewire:pandemic-state.health-care :state="$state" />--}}
-{{--        <livewire:pandemic-state.graph :state="$state" />--}}
+        <livewire:pandemic-state.dashboard :state="$state" lazy />
+        <livewire:pandemic-state.health-care :state="$state" lazy />
+        <livewire:pandemic-state.graph state="{{ $state }}" />
     </div>
 @endsection
 
