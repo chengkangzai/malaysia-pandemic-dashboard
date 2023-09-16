929eee80174748299ef9e1bc8fd675edcb6ca581
Ching Cheng Kang <pycck@hotmail.com>
2023-09-16 23:42:55 +0800

    done malaysia case

---
+++ app/Filament/Resources/CasesMalaysiaResource/Pages/ManageCasesMalaysias.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\CasesMalaysiaResource\Pages;
+
+use App\Filament\Resources\CasesMalaysiaResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageCasesMalaysias extends ManageRecords
+{
+    protected static string $resource = CasesMalaysiaResource::class;
+}
